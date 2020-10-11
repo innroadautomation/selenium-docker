913d208c8826eb73c0026773a9b8db9d63839c02
Surender Avula <surender.avula@innRoad.com>
2020-10-11 16:40:45 +0530

    first commit

---
+++ src/test/java/com/tests/BaseTest.java
@@ -0,0 +1,54 @@
+package com.tests;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeTest
+    public void setupDriver(ITestContext ctx) throws MalformedURLException {
+        // BROWSER => chrome / firefox
+        // HUB_HOST => localhost / 10.0.1.3 / hostname
+    	
+//    	System.setProperty("webdriver.chrome.driver", "D:\\InnRoadAutomation\\Repos\\Workspaces\\workspace-photon\\SeleniumDockerJenkinsProj\\drivers\\chromedriver.exe");
+//    	this.driver=new ChromeDriver();
+    	
+        String host = "localhost";
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
+
+        if(System.getProperty("BROWSER") != null &&
+                System.getProperty("BROWSER").equalsIgnoreCase("firefox")){
+            dc = DesiredCapabilities.firefox();
+        }
+
+        if(System.getProperty("HUB_HOST") != null){
+            host = System.getProperty("HUB_HOST");
+        }
+        
+        String testName = ctx.getCurrentXmlTest().getName();
+        String completeUrl = "http://" + host + ":4444/wd/hub";
+        dc.setCapability("name",testName);
+//        ChromeOptions chromeOptions = new ChromeOptions();      
+//       chromeOptions.addArguments("--disable-dev-shm-usage"); 
+//        dc.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        this.driver = new RemoteWebDriver(new URL(completeUrl), dc);
+        
+    }
+
+    @AfterTest
+    public void quitDriver(){
+        this.driver.quit();
+    }
+
+
+
+}
